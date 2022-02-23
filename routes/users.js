cb4eb3e12a9c888f099d84ba294b845346cdcd5c
Andrea Lorena Castro Manzano <91234415+Lcastro98@users.noreply.github.com>
2022-02-23 15:25:20 -0500

    La ruta funciona

--- routes/users.js
+++ routes/users.js
@@ -6,7 +6,7 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
-router.get('/cool', function(req, res, next) {
+router.get('/cool/', function(req, res, next) {
   res.render('index', { cool: 'Eres genial' });
 });
 
